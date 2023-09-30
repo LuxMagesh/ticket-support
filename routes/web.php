675ce9e7311e92e03162c7fd13c6e48aed16382b
325iiiSGuSH <luxmotsumi67@gmail.com>
2023-09-30 19:38:17 +0200

    Web Routes Custom Redirect

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,17 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TicketController;
 
+route::get('/', [TicketController::class, 'redirect']);
+route::get('/redirect', [TicketController::class, 'redirect'])->name('redirect');;
+route::get('/tickets', [TicketController::class, 'index'])->name('index');
+route::get('/tickets/{ticket}', [TicketController::class, 'show'])->name('show');
+route::post('/tickets/{ticket}', [TicketController::class, 'update'])->name('update');
+route::get('/create', [TicketController::class, 'create'])->name('create');
+route::post('/create', [TicketController::class, 'add'])->name('add');
+route::get('/complex', [TicketController::class, 'complex'])->name('complex');
+route::get('/tickets/delete/{ticket}', [TicketController::class, 'delete'])->name('delete');
+route::post('/tickets', [TicketController::class, 'filter'])->name('filter');
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
@@ -13,14 +24,5 @@ Route::middleware([
     })->name('dashboard');
 });
 
-route::get('/redirect', [TicketController::class, 'redirect']);
-route::get('/tickets', [TicketController::class, 'index'])->name('tickets');
-route::get('/tickets/{ticket}', [TicketController::class, 'show'])->name('show');
-route::post('/tickets/{ticket}', [TicketController::class, 'update'])->name('update');
-route::get('/create', [TicketController::class, 'create'])->name('create');
-route::post('/create', [TicketController::class, 'add'])->name('add');
-route::get('/complex', [TicketController::class, 'complex'])->name('complex');
-route::get('/tickets/delete/{ticket}', [TicketController::class, 'delete'])->name('delete');
-route::post('/tickets', [TicketController::class, 'filter'])->name('filter');
 
 
